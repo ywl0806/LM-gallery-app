6236963be8abffa79621661857069e1eb1d6142e
イヨンウ <lee.yong.woo@arsaga.jp>
2024-04-01 09:17:16 +0900

    Refactor LivePhotoLocal component

--- src/components/blocks/LivePhotoLocal.tsx
+++ src/components/blocks/LivePhotoLocal.tsx
@@ -1,6 +1,6 @@
 import {PhotoIdentifier} from '@react-native-camera-roll/camera-roll';
 import {useQuery} from '@tanstack/react-query';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
 import {getLivePhotoMov} from '../../utils/getLivePhotoMov';
 import {LivePhotoView} from './LivePhotoView';
@@ -10,17 +10,10 @@ type Props = {
 };
 
 export const LivePhotoLocal = ({photo}: Props) => {
-  const [liveUrlState, setLiveUrlState] = useState('');
-
   const {data} = useQuery({
     queryKey: ['liveUrl', photo.node.id],
     queryFn: () => getLivePhotoMov(photo.node.id),
   });
-  useEffect(() => {
-    if (data) setLiveUrlState(data);
-  }, [data]);
 
-  return (
-    <LivePhotoView photoUrl={photo.node.image.uri} liveUrl={liveUrlState} />
-  );
+  return <LivePhotoView photoUrl={photo.node.image.uri} liveUrl={data ?? ''} />;
 };
